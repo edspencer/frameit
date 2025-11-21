16803901c6b17d48960984045d2cb51ce8235839
Ed Spencer <ed@edspencer.net>
2025-11-21 09:08:49 -0500

    permissions

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 1,
-  workers: process.env.CI ? 8 : 24,
+  workers: process.env.CI ? 4 : 24,
 
   // Reporter configuration
   reporter: [
